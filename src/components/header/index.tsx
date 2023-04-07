fad4040d9e14edc7ec42b615bc5aeef5ed79c0c0
SamirAjdini <ajdinis1993@gmail.com>
2023-04-07 14:53:14 +0200

    added Header component, implemented Title on it

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header--title">
+        <h1>Find a Book</h1>
+      </div>
+      <div className="header--filter"></div>
+    </div>
+  );
+};
+
+export default Header;
